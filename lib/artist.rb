b247df192aaf6074352f5dc754ee2c43e9a6d7db
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-29 14:37:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,21 @@
-require 'pry'
-  include Paramable
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-
 class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
   def initialize
-    super
+    @@artists << self
     @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -20,14 +24,11 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-#  end
 end
